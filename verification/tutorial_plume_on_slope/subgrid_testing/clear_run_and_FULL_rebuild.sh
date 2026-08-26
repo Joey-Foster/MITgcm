c5cce63f9fc6b156e0c5ee36745a423882e61424
joey <jlf1g19@soton.ac.uk>
2026-08-26 14:59:58 +0100

    diffusion homog maybe working?

---
+++ verification/tutorial_plume_on_slope/subgrid_testing/clear_run_and_FULL_rebuild.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -r run/*
+cd build
+make CLEAN && \
+~/Documents/MITgcm/tools/genmake2 -rootdir=$HOME/Documents/MITgcm/ -mods=../code -of=$HOME/Documents/MITgcm/tools/build_options/linux_amd64_gfortran && \
+make depend && \
+make -j 3 
--- verification/tutorial_plume_on_slope/subgrid_testing/clear_run_and_rebuild.sh
+++ verification/tutorial_plume_on_slope/subgrid_testing/clear_run_and_rebuild.sh
@@ -2,7 +2,6 @@
 
 rm -r run/*
 cd build
-make CLEAN && \
-~/Documents/MITgcm/tools/genmake2 -rootdir=$HOME/Documents/MITgcm/ -mods=../code -of=$HOME/Documents/MITgcm/tools/build_options/linux_amd64_gfortran && \
+make Clean && \
 make depend && \
 make -j 3 
