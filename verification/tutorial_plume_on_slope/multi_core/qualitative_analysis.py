b8b3ac28cb4e3746dd0b6636cb3f1fa0a03ddf2f
J.Foster <jlf1g19@login6002.head.cm.local>
2026-09-24 15:48:56 +0100

    reworked qualitative analysis to produce 3-way comparison figure

--- verification/tutorial_plume_on_slope/multi_core/qualitative_analysis.py
+++ verification/tutorial_plume_on_slope/multi_core/qualitative_analysis.py
@@ -5,15 +5,12 @@ import matplotlib.pyplot as plt
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-s', '--smooth', help='file path to state_global.nc with smooth ' 
-                    'bathymetry and which does not use wall functions', required=True)
-parser.add_argument('-swf', '--smooth+wallfuncs', help='file path to state_global.nc with smooth ' 
-                    'bathymetry and which uses wall functions', dest='swf')
-parser.add_argument('-w', '--wiggly', help='file path to state_global.nc with '
-                    'multiscale bathymetry and which does not use wall functions',
-                    required=True)
-parser.add_argument('-wwf', '--wiggly+wallfuncs', help='file path to state_global.nc with '
-                    'multiscale bathymetry and which uses wall functions', dest='wwf')
+parser.add_argument('-t', '--truth', help='file path to state_global.nc with high res ' 
+                    'multiscale bathymetry, represneting the "truth"', required=True)
+parser.add_argument('--off', help='file path to state_global.nc with smooth coarse '
+                    'bathymetry and WITHOUT my custom package', required=True)
+parser.add_argument('--on', help='file path to state_global.nc with smooth coarse '
+                    'bathymetry and WITH my custom package', required=True)
 args=parser.parse_args()
 
 datasets = {}
@@ -25,60 +22,38 @@ for flag, path in vars(args).items():
 
 if len(set([datasets[flag]['T'].values[-1] for flag, path in vars(args).items() if path is not None])) != 1:
     print('Warning: Datasets do not share a common end time.\n'
-          'Defaulting to "smooth" dataset time axis.')
-time = datasets['smooth']['T'].values
+          'Defaulting to "highres" dataset time axis.')
+time = datasets['truth']['T'].values
 
-vmin = thetas['smooth'].isel(T=-1).min().values
-vmax = thetas['smooth'].isel(T=-1).max().values
+vmin = thetas['truth'].isel(T=-1).min().values
+vmax = thetas['truth'].isel(T=-1).max().values
 
 plotting_customisation = {'cmap': 'jet',
-                          'vmin': vmin, 
-                          'vmax': vmax,
+                          #'vmin': vmin, 
+                           'vmin': -0.08,
+                          #'vmax': vmax,
+                           'vmax': 0.005,
                           'add_colorbar': False
                           }
-if (args.swf is not None) and (args.wwf is not None):
-    fig, ax = plt.subplots(2,2, constrained_layout=True, sharex=True, sharey=True, figsize=(8,6))
-    im = thetas['smooth'].isel(T=-1).plot(ax=ax[0,0], **plotting_customisation)
-    ax[0,0].set_xlabel('')
-    ax[0,0].set_ylabel('Depth [m]')
-    ax[0,0].set_title('Smooth bathymetry')
 
-    thetas['wiggly'].isel(T=-1).plot(ax=ax[0,1], **plotting_customisation)
-    ax[0,1].set_xlabel('')
-    ax[0,1].set_ylabel('')
-    ax[0,1].set_title('Multiscale bathymetry')
-    ax[0,1].text(1.02, 0.5, "No wall functions", 
-                 transform=ax[0,1].transAxes, rotation=-90, va="center", ha="left", fontsize=12)
-    
-    thetas['swf'].isel(T=-1).plot(ax=ax[1,0], **plotting_customisation)
-    ax[1,0].set_xlabel('X [m]')
-    ax[1,0].set_ylabel('Depth [m]')
-    ax[1,0].set_title('')
-    
-    thetas['wwf'].isel(T=-1).plot(ax=ax[1,1], **plotting_customisation)
-    ax[1,1].set_xlabel('X [m]')
-    ax[1,1].set_ylabel('')
-    ax[1,1].set_title('')
-    ax[1,1].text(1.02, 0.5, "Wall functions", 
-                 transform=ax[1,1].transAxes, rotation=-90, va="center", ha="left", fontsize=12)
-    
-else:
-    fig, ax = plt.subplots(1, 2, constrained_layout=True, sharey=True)
-    im = thetas['smooth'].isel(T=-1).plot(ax=ax[0], **plotting_customisation)
-    ax[0].set_xlabel('')
-    ax[0].set_ylabel('Depth [m]')
-    ax[0].set_title('Smooth bathymetry')
-    
-    thetas['wiggly'].isel(T=-1).plot(ax=ax[1], **plotting_customisation)
-    ax[1].set_xlabel('')
-    ax[1].set_ylabel('')
-    ax[1].set_title('Multiscale bathymetry')
+fig, ax = plt.subplots(1, 3, constrained_layout=True, sharey=True, figsize=(10,5))
+im = thetas['truth'].isel(T=-1).plot(ax=ax[0], **plotting_customisation)
+ax[0].set_xlabel('X [m]')
+ax[0].set_ylabel('Depth [m]')
+ax[0].set_title('Multiscale bathymetry\n("truth")')
+
+thetas['off'].isel(T=-1).plot(ax=ax[1], **plotting_customisation)
+ax[1].set_xlabel('X [m]')
+ax[1].set_ylabel('')
+ax[1].set_title('Smooth bathymetry\n (homog OFF)')
+
+thetas['on'].isel(T=-1).plot(ax=ax[2], **plotting_customisation)
+ax[2].set_xlabel('X [m]')
+ax[2].set_ylabel('')
+ax[2].set_title('Smooth bathymetry\n (homog ON)')
     
 cbar = fig.colorbar(im, ax=ax, orientation="horizontal")
 cbar.set_label(r'$\theta$ [degC]')
 plt.suptitle(rf'Potential tempertaure at $t={{ {int(time[-1])} }}$s')
 
-if (args.swf is not None) and (args.wwf is not None):
-    plt.savefig('bathymetry_comparison_2x2.pdf', bbox_inches='tight')
-else:
-    plt.savefig('bathymetry_comparison.pdf', bbox_inches='tight')
+plt.savefig('homogenisation_comparison.pdf', bbox_inches='tight')
